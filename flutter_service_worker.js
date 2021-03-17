614152a750f8484b83f3870a86bc1008a3a84d91
ko50 <ko50@users.noreply.github.com>
2021-03-17 18:19:21 +0000

    Automated deployment: Wed Mar 17 18:19:21 UTC 2021 ab007ef89d50aa79934597671d23cc02dff2232c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -15,8 +15,8 @@ const RESOURCES = {
 "version.json": "426313f2f3133c2f20415344c4a22df3",
 "manifest.json": "e79281ec7b9b78bb54c495866ac746d5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "eb06ebf077009e5714c9d602da02bcb4",
-"/": "eb06ebf077009e5714c9d602da02bcb4"
+"index.html": "62990e7e098a7acb0699ded060b4d2c4",
+"/": "62990e7e098a7acb0699ded060b4d2c4"
 };
 
 // The application shell files that are downloaded before a service worker can
