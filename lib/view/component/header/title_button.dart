0a2ed10c98572c20c39e21c27d9afe09a09bb2b8
ko5 <kotyana1331@gmail.com>
2021-03-19 01:44:06 +0900

    [追加] タイトルボタンでの遷移機能を実装

--- lib/view/component/header/title_button.dart
+++ lib/view/component/header/title_button.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:portfolio/helper/sections.dart';
 import 'package:portfolio/helper/theme_colors.dart';
+import 'package:portfolio/provider.dart';
 
 class TitleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {},
-      child: Text(
-        Section.values[0].text,
-        style: TextStyle(
-          fontSize: 23,
-          fontWeight: FontWeight.w700,
-          color: ThemeColor.Background.color,
+    return Consumer(builder: (context, watch, _) {
+      final notifier = watch(displayedSectionIndex);
+
+      return GestureDetector(
+        onTap: () => notifier.value = 0,
+        child: Text(
+          Section.values[0].text,
+          style: TextStyle(
+            fontSize: 23,
+            fontWeight: FontWeight.w700,
+            color: ThemeColor.Background.color,
+          ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
