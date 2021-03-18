46d92f67c63377cb61bac79fddfc0e0da76399f0
ko5 <kotyana1331@gmail.com>
2021-03-18 22:35:50 +0900

    [修正] Providerの型を変更

--- lib/provider.dart
+++ lib/provider.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final ChangeNotifierProvider<ValueNotifier<int?>> hoveredSectionIndex =
-    ChangeNotifierProvider((_) => ValueNotifier<int?>(null));
+final ChangeNotifierProvider<ValueNotifier<int>> hoveredSectionIndex =
+    ChangeNotifierProvider((_) => ValueNotifier<int>(-1));
 
 final ChangeNotifierProvider<ValueNotifier<int>> displayedSectionIndex =
     ChangeNotifierProvider((_) => ValueNotifier<int>(0));
