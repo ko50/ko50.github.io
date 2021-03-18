99338218202059b3b67786b15c39c44575d4a5f7
ko5 <kotyana1331@gmail.com>
2021-03-18 23:04:48 +0900

    [追加] ルーティング(の真似事)を仮実装した

--- lib/view/section_router.dart
+++ lib/view/section_router.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:portfolio/helper/sections.dart';
+import 'package:portfolio/provider.dart';
+import 'package:portfolio/view/component/root/root.dart';
+
 class SectionRouter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer(
+      builder: (context, watch, _) {
+        Section displayedSection =
+            Section.values[watch(displayedSectionIndex).value];
+
+        switch (displayedSection) {
+          case Section.root:
+            return Root();
+          case Section.aboutMe:
+            return Root();
+          case Section.skills:
+            return Root();
+          case Section.works:
+            return Root();
+          case Section.contacts:
+            return Root();
+        }
+      },
+    );
   }
 }
