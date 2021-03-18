b5dbea4c726727779cae5dc019ba2d9f30523c1a
ko5 <kotyana1331@gmail.com>
2021-03-18 18:56:54 +0900

    [追加] ボタンのホバー情報を保持するproviderを定義

---
+++ lib/provider.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final ChangeNotifierProvider<ValueNotifier<int?>> hoveredSectionIndex =
+    ChangeNotifierProvider((_) => ValueNotifier<int?>(null));
+
+final ChangeNotifierProvider<ValueNotifier<int>> displayedSectionIndex =
+    ChangeNotifierProvider((_) => ValueNotifier<int>(0));
