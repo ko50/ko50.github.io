84079a35b36d5054a9bdd8023e9cc4c58db25249
ko5 <kotyana1331@gmail.com>
2021-03-22 16:39:42 +0900

    [追加] contactway

---
+++ lib/data/contacts.dart
@@ -0,0 +1,26 @@
+enum ContactWay {
+  Twitter,
+  GitHub,
+}
+
+extension ContactWayExtension on ContactWay {
+  String get name {
+    switch (this) {
+      case ContactWay.Twitter:
+        return 'Twitter';
+      case ContactWay.GitHub:
+        return 'GitHub';
+    }
+  }
+
+  String get userName {
+    switch (this) {
+      case ContactWay.Twitter:
+        return '@ko_CottonLove';
+      case ContactWay.GitHub:
+        return '@ko50';
+    }
+  }
+
+  String get logoPath => 'assets/images/contacts/${this.name}.svg';
+}
