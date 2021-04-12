04fbb5863e3310377646b0414238931384266a35
ko5 <kotyana1331@gmail.com>
2021-04-12 15:22:21 +0900

    [追加] skillsのmodel

--- lib/model/about_me.dart
+++ lib/model/about_me.dart
@@ -9,17 +9,17 @@ class AboutMeData {
   final String title;
   final String content;
 
-  static const String name = 'about_me';
+  static const String nameSpace = 'about_me';
 
   static Future<List<AboutMeData>?> fetchAll() async {
     print('AboutMeData: Fetcing data');
-    final Uri url = Uri.parse('$apiUriRoot/$name');
+    final Uri url = Uri.parse('$apiUriRoot/$nameSpace');
     final http.Response response = await http.get(url);
 
     print('AboutMeData: ${response.statusCode}');
 
     if (response.statusCode != HttpStatus.ok)
-      throw Exception('$name: Failed to fetch data');
+      throw Exception('$nameSpace: Failed to fetch data');
 
     final List<dynamic> dataJson = json.decode(response.body);
     final List<AboutMeData> data = dataJson
---
+++ lib/model/skills.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+import 'package:portfolio/constants.dart';
+import 'package:portfolio/model/enum/skill_type.dart';
+
+class SkillData {
+  final String name;
+  final String description;
+  final String logoPath;
+  final SkillType type;
+
+  static const String nameSpace = 'skills';
+
+  static Future<List<SkillData>?> fetchAll() async {
+    print('SkillData: Fetcing data');
+    final Uri url = Uri.parse('$apiUriRoot/$nameSpace');
+    final http.Response response = await http.get(url);
+
+    print('SkillData: ${response.statusCode}');
+
+    if (response.statusCode != HttpStatus.ok)
+      throw Exception('$nameSpace: Failed to fetch data');
+
+    final List<dynamic> dataJson = json.decode(response.body);
+    final List<SkillData> data = dataJson
+        .map<SkillData>(
+          (e) => SkillData._fromJson(e),
+        )
+        .toList();
+
+    return data;
+  }
+
+  SkillData._fromJson(Map<String, dynamic> json)
+      : this.name = json['name'],
+        this.description = json['description'],
+        this.logoPath = json['logo_path'],
+        this.type = json['skill_type'];
+}
