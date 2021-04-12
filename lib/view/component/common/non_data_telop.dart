96c5f0558c5b555cbd496a635d999dfcd2999e6b
ko5 <kotyana1331@gmail.com>
2021-04-12 22:23:19 +0900

    [追加] non data telop

---
+++ lib/view/component/common/non_data_telop.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/helper/theme_colors.dart';
+
+class NonDataTelop extends StatelessWidget {
+  final String? message;
+
+  const NonDataTelop({this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        message ?? 'データがありません',
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.w300,
+          color: ThemeColor.Grey.color,
+        ),
+      ),
+    );
+  }
+}
