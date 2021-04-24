284652abd2eae748bbb81d9127e6eb198310f6d4
ko5 <kotyana1331@gmail.com>
2021-04-25 04:36:44 +0900

    [修正] localhost

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 const double widthBreakpoint = 630;
 
 const int transitionDefaultDuration = 400;
@@ -8,4 +10,4 @@ const Duration footerCloseDelay = Duration(milliseconds: 700);
 
 const Duration drawerAnimateDuration = Duration(milliseconds: 200);
 
-const String apiUriRoot = 'http://localhost:8000/api/v1';
+final String apiUriRoot = Platform.environment['PORTFOLIO_API_HOST'] ?? '';
