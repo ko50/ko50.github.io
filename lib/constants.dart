9368cdda53fc68e46dfedb93295e14dd4ab9456f
ko5 <kotyana1331@gmail.com>
2021-04-25 04:46:12 +0900

    [修正] nanimitennda.com

--- lib/constants.dart
+++ lib/constants.dart
@@ -10,4 +10,5 @@ const Duration footerCloseDelay = Duration(milliseconds: 700);
 
 const Duration drawerAnimateDuration = Duration(milliseconds: 200);
 
-final String apiUriRoot = Platform.environment['PORTFOLIO_API_HOST'] ?? '';
+final String apiUriRoot =
+    Platform.environment['PORTFOLIO_API_HOST'] ?? 'https://nanimitenda.com';
