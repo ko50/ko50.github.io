15f5c5d1a4d64e0ce8010c79ba617acf40a377f2
ko50 <kotyana1331@gmail.com>
2021-05-03 18:22:50 +0900

    [変更] AboutMeでTransitionを使うようにデザインを修正

--- lib/view/sections/about_me/information_tile.dart
+++ lib/view/sections/about_me/information_tile.dart
@@ -8,13 +8,36 @@ import 'package:portfolio/enum/theme_colors.dart';
 import 'package:portfolio/model/about_me.dart';
 import 'package:portfolio/providers.dart';
 
-class InformationTile extends StatelessWidget {
+class InformationTile extends StatefulWidget {
   final AboutMeData data;
   final int index;
 
-  static const double translateXOffset = 32.0;
+  static const double maxWidth = 300;
 
   InformationTile({required this.data, required this.index});
+
+  @override
+  _InformationTileState createState() => _InformationTileState();
+}
+
+class _InformationTileState extends State<InformationTile>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: Duration(milliseconds: transitionDefaultDuration * 2),
+      vsync: this,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,60 +45,105 @@ class InformationTile extends StatelessWidget {
       builder: (context, watch, _) {
         AnimationType visibleState = watch(animationNotifier).value;
 
-        return AnimatedOpacity(
-          duration: Duration(milliseconds: transitionDefaultDuration),
-          curve: Interval(index * 0.1, (1.0 - (4 * 0.1)) + (index * 0.1)),
-          opacity: visibleState == AnimationType.appear ? 1 : 0,
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: transitionDefaultDuration),
-            curve: Interval(
-              index * 0.1,
-              (1.0 - (4 * 0.1)) + (index * 0.1),
-              curve: Curves.easeOutCubic,
-            ),
-            constraints: BoxConstraints(minWidth: 100),
-            margin: EdgeInsets.only(
-              bottom: 16.0,
-              left: visibleState == AnimationType.appear ? translateXOffset : 0,
-            ),
-            padding: EdgeInsets.only(left: 8.0, right: translateXOffset),
-            decoration: BoxDecoration(
-              border: Border(
-                  left: BorderSide(
-                      width: 6.0, color: ThemeColor.PurpleBlack.color)),
-            ),
-            child: _content(),
+        visibleState == AnimationType.appear
+            ? _controller.forward()
+            : _controller.reverse();
+
+        return Container(
+          margin: EdgeInsets.symmetric(vertical: 8.0),
+          width: InformationTile.maxWidth,
+          child: Stack(
+            children: [
+              SizeTransition(
+                axis: Axis.horizontal,
+                sizeFactor: CurvedAnimation(
+                  curve: Interval(
+                    (widget.index + 2) * 0.1,
+                    (1.0 - (4 * 0.1)) + (widget.index * 0.1),
+                    curve: Curves.easeOutQuint,
+                  ),
+                  parent: _controller,
+                ),
+                child: _content(),
+              ),
+              Positioned(
+                top: 0,
+                bottom: 0,
+                child: SizeTransition(
+                  axis: Axis.horizontal,
+                  sizeFactor: Tween<double>(
+                    begin: 1.0,
+                    end: 0.0,
+                  ).animate(
+                    CurvedAnimation(
+                      curve: Interval(
+                        (widget.index + 3) * 0.1,
+                        (1.0 - (4 * 0.1)) + (widget.index * 0.1),
+                        curve: Curves.easeOutCirc,
+                      ),
+                      parent: _controller,
+                    ),
+                  ),
+                  child: SizeTransition(
+                    axis: Axis.horizontal,
+                    axisAlignment: 1.0,
+                    sizeFactor: Tween<double>(begin: 0, end: 1).animate(
+                      CurvedAnimation(
+                        curve: Interval(
+                          widget.index * 0.1,
+                          (1.0 - (4 * 0.1)) + (widget.index * 0.1),
+                          curve: Curves.easeOutCirc,
+                        ),
+                        parent: _controller,
+                      ),
+                    ),
+                    child: Container(
+                      color: ThemeColor.PurpleBlack.color,
+                      width: InformationTile.maxWidth,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         );
       },
     );
   }
 
-  Column _content() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          data.title,
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.w700,
-            color: ThemeColor.PurpleBlack.color,
-          ),
+  Container _content() {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8.0),
+      // constraints: BoxConstraints(maxWidth: InformationTile.maxWidth),
+      width: InformationTile.maxWidth,
+      decoration: BoxDecoration(
+        border: Border(
+          left: BorderSide(width: 6.0, color: ThemeColor.PurpleBlack.color),
+          bottom: BorderSide(width: 1.0, color: ThemeColor.PurpleBlack.color),
         ),
-        FittedBox(
-          fit: BoxFit.fitWidth,
-          child: Text(
-            data.content,
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            widget.data.title,
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: ThemeColor.PurpleBlack.color,
+            ),
+          ),
+          Text(
+            widget.data.content,
             style: TextStyle(
               fontSize: 15,
               fontWeight: FontWeight.w500,
               color: Colors.black,
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
