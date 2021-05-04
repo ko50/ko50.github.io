6c6d1d8ab3dfdea55cd7d4ddbfeabe4f60d880ae
ko50 <kotyana1331@gmail.com>
2021-05-05 00:23:28 +0900

    [変更] Contactsのアニメーションを編集

--- lib/view/sections/contacts/contact_card.dart
+++ lib/view/sections/contacts/contact_card.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:flutter/rendering.dart' show SystemMouseCursors;
 
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:portfolio/constants.dart';
-import 'package:portfolio/enum/animation_type.dart';
 import 'package:portfolio/enum/theme_colors.dart';
 import 'package:portfolio/model/contacts.dart';
-import 'package:portfolio/providers.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactCard extends StatefulWidget {
@@ -26,11 +23,7 @@ class ContactCard extends StatefulWidget {
 
 class _ContactCardState extends State<ContactCard>
     with SingleTickerProviderStateMixin {
-  late final AnimationController _transitionAnimationController =
-      AnimationController(
-    vsync: this,
-    duration: Duration(seconds: 1),
-  );
+  late final AnimationController _controller;
 
   void _launchLink() async {
     String url = widget.contact.serviceLink;
@@ -39,78 +32,128 @@ class _ContactCardState extends State<ContactCard>
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+  }
+
+  @override
   void dispose() {
-    _transitionAnimationController.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      cursor: SystemMouseCursors.click,
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: _launchLink,
-        child: Consumer(
-          builder: (context, watch, _) {
-            bool visibility =
-                watch(animationNotifier).value == AnimationType.appear;
-
-            visibility
-                ? _transitionAnimationController.forward()
-                : _transitionAnimationController.reverse();
-
-            return AnimatedOpacity(
-              duration: Duration(milliseconds: transitionDefaultDuration),
-              opacity: visibility ? 1 : 0,
-              child: Card(
-                elevation: 3,
-                child: AnimatedContainer(
-                  duration: Duration(milliseconds: transitionDefaultDuration),
-                  curve: Curves.easeInOutBack,
-                  transform:
-                      Matrix4.diagonal3Values(visibility ? 1 : 0.1, 1, 1),
-                  margin: EdgeInsets.symmetric(vertical: 32.0),
-                  padding: EdgeInsets.symmetric(horizontal: 32.0),
+    return SizeTransition(
+      axis: Axis.horizontal,
+      sizeFactor: CurvedAnimation(
+        curve: Interval(0.0, 0.4, curve: Curves.easeOutQuint),
+        reverseCurve: Interval(0.2, 0.6, curve: Curves.easeInQuint),
+        parent: widget.animation,
+      ),
+      child: MouseRegion(
+        cursor: SystemMouseCursors.click,
+        onEnter: (_) => _controller.forward(),
+        onExit: (_) => _controller.reverse(),
+        child: GestureDetector(
+          behavior: HitTestBehavior.opaque,
+          onTap: _launchLink,
+          child: Stack(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 32.0),
+                constraints: BoxConstraints(maxWidth: 400),
+                decoration: BoxDecoration(
+                  border: Border.all(color: ThemeColor.PurpleBlack.color),
+                ),
+                child: Container(
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.start,
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [_logo(visibility), _userName()],
+                    children: [_logo(), _userName()],
                   ),
                 ),
               ),
-            );
-          },
+              Positioned(
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                child: FadeTransition(
+                  opacity: Tween(begin: 1.0, end: 0.0).animate(
+                    CurvedAnimation(
+                      curve: Curves.easeOutQuint,
+                      reverseCurve: Curves.easeInQuint,
+                      parent: widget.animation,
+                    ),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: ThemeColor.PurpleBlack.color,
+                      border: Border.all(color: ThemeColor.PurpleBlack.color),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  SafeArea _logo(bool visibility) {
-    return SafeArea(
-      child: RotationTransition(
-        turns: CurvedAnimation(
-          parent: _transitionAnimationController,
-          curve: Curves.easeInOutBack,
+  Widget _logo() {
+    return RotationTransition(
+      turns: Tween(begin: 0.0, end: 0.2).animate(
+        CurvedAnimation(
+          curve: Curves.bounceOut,
+          reverseCurve: Curves.bounceIn,
+          parent: _controller,
         ),
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: transitionDefaultDuration - 100),
-          curve: Curves.easeInExpo,
-          height: visibility ? 40 : 0,
-          width: visibility ? 40 : 0,
+      ),
+      child: Container(
+        width: 40,
+        height: 40,
+        child: ScaleTransition(
+          scale: CurvedAnimation(
+            curve: Curves.easeOutCirc,
+            reverseCurve: Curves.easeInCirc,
+            parent: widget.animation,
+          ),
           child: SvgPicture.asset(widget.contact.logoPath),
         ),
       ),
     );
   }
 
-  Padding _userName() {
-    return Padding(
-      padding: EdgeInsets.only(left: 16.0),
-      child: AnimatedDefaultTextStyle(
-        duration: Duration(milliseconds: transitionDefaultDuration - 100),
-        style: TextStyle(fontSize: 25, color: ThemeColor.DeepGrey.color),
-        child: Text(widget.contact.userName),
+  Widget _userName() {
+    return FadeTransition(
+      opacity: CurvedAnimation(
+        curve: Interval(0.4, 1.0, curve: Curves.easeInOutExpo),
+        parent: widget.animation,
+      ),
+      child: SizeTransition(
+        axis: Axis.horizontal,
+        axisAlignment: 1.0,
+        sizeFactor: CurvedAnimation(
+          curve: Interval(0.4, 1.0, curve: Curves.easeInOutExpo),
+          parent: widget.animation,
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 32.0),
+          child: Text(
+            widget.contact.userName,
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w300,
+              color: ThemeColor.Grey.color,
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/view/sections/contacts/contacts.dart
+++ lib/view/sections/contacts/contacts.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/enum/sections.dart';
+import 'package:portfolio/constants.dart';
 
+import 'package:portfolio/enum/sections.dart';
 import 'package:portfolio/model/contacts.dart';
 import 'package:portfolio/view/sections/contacts/contact_card.dart';
 import 'package:portfolio/view/sections/section_container.dart';
@@ -10,20 +11,19 @@ class Contacts extends StatelessWidget {
   Widget build(BuildContext context) {
     return SectionContainer(
       section: Section.contacts,
+      appearDuration: Duration(milliseconds: transitionDefaultDuration * 3),
       builder: (data, animation) {
         assert(data.every((e) => e is ContactData));
 
-        return Container(
-          constraints: BoxConstraints(maxWidth: 400),
-          child: ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, index) {
-              return ContactCard(
-                contact: data[index] as ContactData,
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            for (int i = 0; i < data.length; i++)
+              ContactCard(
+                contact: data[i] as ContactData,
                 animation: animation,
-              );
-            },
-          ),
+              ),
+          ],
         );
       },
     );
