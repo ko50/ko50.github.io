b10f20412a67d059394375bdce3c80617ab53c1f
ko50 <kotyana1331@gmail.com>
2021-07-08 11:48:21 +0900

    [変更] アニメーションをゆっくりにした

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,14 +4,14 @@ import 'dart:io';
 
 const double widthBreakpoint = 630;
 
-const int transitionDefaultDuration = 400;
+const int transitionDefaultDuration = 500;
 
 const Duration initialLoadDuration = Duration(milliseconds: 900);
 
 const Duration footerAnimateDuration = Duration(milliseconds: 700);
 
-const Duration footerCloseDelay = Duration(milliseconds: 700);
+const Duration footerCloseDelay = Duration(milliseconds: 900);
 
-const Duration drawerAnimateDuration = Duration(milliseconds: 200);
+const Duration drawerAnimateDuration = Duration(milliseconds: 250);
 
 final String apiUriRoot = 'https://api.ko50.dev/api/v1';
