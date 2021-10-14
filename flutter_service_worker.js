c6a14120089382a47e866d064681cbcfbeb182e2
ko50 <ko50@users.noreply.github.com>
2021-10-14 17:36:07 +0000

    Automated deployment: Thu Oct 14 17:36:07 UTC 2021 73317214249d483fb701accefa699f2d48c7cb74

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,27 +3,27 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "426313f2f3133c2f20415344c4a22df3",
-"main.dart.js": "8cb87f9ad60e3713313bcb1c5e010285",
+  "CNAME": "7e3c23e6399887987831e82dd485a875",
 "manifest.json": "84fa74280f2b60578ee975e2dec345d3",
+"version.json": "426313f2f3133c2f20415344c4a22df3",
+"main.dart.js": "dfd407f9507822f5a44fac2222235c33",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/FontManifest.json": "85e4a8d67b6378dbf543a8cc101107b6",
 "assets/images/ko.jpg": "fbc3e88f12912063bf03050781243700",
 "assets/images/cotton_circle.png": "8d73fba78ea832930fb9586e8205c213",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/AssetManifest.json": "d7f59c645e698ef2a60b4804460c0deb",
-"assets/NOTICES": "ac35dd5e4918c4e5e5e5d0129fc4bc39",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/fonts/Roboto-Bold.ttf": "e07df86cef2e721115583d61d1fb68a6",
 "assets/fonts/Roboto-Regular.ttf": "11eabca2251325cfc5589c9c6fb57b46",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/fonts/Roboto-Black.ttf": "5ebb24ee1112dd9562629375c387879a",
 "assets/fonts/Roboto-Medium.ttf": "58aef543c97bbaf6a9896e8484456d98",
-"assets/fonts/Roboto-Light.ttf": "88823c2015ffd5fa89d567e17297a137",
+"assets/fonts/Roboto-Bold.ttf": "e07df86cef2e721115583d61d1fb68a6",
 "assets/fonts/Roboto-Thin.ttf": "321de678e592d0b8f44f1a82d7ca4b62",
-"assets/FontManifest.json": "85e4a8d67b6378dbf543a8cc101107b6",
+"assets/fonts/Roboto-Light.ttf": "88823c2015ffd5fa89d567e17297a137",
+"assets/NOTICES": "ac35dd5e4918c4e5e5e5d0129fc4bc39",
+"assets/AssetManifest.json": "d7f59c645e698ef2a60b4804460c0deb",
 "assets/assets/images/ko.jpg": "fbc3e88f12912063bf03050781243700",
 "assets/assets/images/cotton_circle.png": "8d73fba78ea832930fb9586e8205c213",
-"index.html": "423a7f485a32b11c740fecb805e9fff1",
-"/": "423a7f485a32b11c740fecb805e9fff1",
-"CNAME": "7e3c23e6399887987831e82dd485a875"
+"index.html": "26762e8e98b57fb045778d686fb8dc89",
+"/": "26762e8e98b57fb045778d686fb8dc89"
 };
 
 // The application shell files that are downloaded before a service worker can
